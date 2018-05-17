5176a9545425f2057b4d5879b4d2a092a98de730
Andrew Tu <drewtu2@yahoo.com>
2018-05-17 14:03:11 -0400

    added gaussian temporal filter, beginning to work on thresholding

--- project1/motion_detector.m
+++ project1/motion_detector.m
@@ -11,7 +11,7 @@ function motion_detector()
     global TEMPORAL_SCALE
     global GAUSS_STD
 
-    THRESHOLD = 35;
+    THRESHOLD = 8;
     WHITE = 255;
     BLACK = 0;
     TEMPORAL_SCALE = 1;
@@ -47,42 +47,116 @@ function processing_pipeline(video)
     global BOX5
     % Start at 2nd image, end at length - 1 because the nth term is the 
     % difference of n-1 and n+1
-    for k = 2 : length(video.Files) - 1
-        previous = readimage(video, k - 1) * TEMPORAL_SCALE;
-        next = readimage(video, k + 1) * TEMPORAL_SCALE;
-        frame.color = readimage(video, k);
+    %for k = 2 : length(video.Files) - 1
+    %    previous = readimage(video, k - 1) * TEMPORAL_SCALE;
+    %    next = readimage(video, k + 1) * TEMPORAL_SCALE;
+    %    frame.color = readimage(video, k);
 
-        [frame.gauss_filter, frame.gauss_mask, frame.gauss_movement] = ...
-            processing_pipeline_helper(frame.color, previous, next, GAUSS);
-        [frame.box3_filter, frame.box3_mask, frame.box3_movement] = ...
-            processing_pipeline_helper(frame.color, previous, next, BOX3);
-        [frame.box5_filter, frame.box5_mask, frame.box5_movement] = ...
-            processing_pipeline_helper(frame.color, previous, next, BOX5);
+    %    [frame.gauss_filter, frame.gauss_mask, frame.gauss_movement] = ...
+    %        processing_pipeline_helper(frame.color, previous, next, GAUSS);
+    %    [frame.box3_filter, frame.box3_mask, frame.box3_movement] = ...
+    %        processing_pipeline_helper(frame.color, previous, next, BOX3);
+    %    [frame.box5_filter, frame.box5_mask, frame.box5_movement] = ...
+    %        processing_pipeline_helper(frame.color, previous, next, BOX5);
 
-        % Display...
-        display_frame(frame);
+    %    % Display...
+    %    display_frame(frame);
+    %end
+
+    % Gaussian 1D
+    % 1/16 * [1 4 6 4 1] over each pixel with respect to time. 
+
+    for k = 3 : length(video.Files) - 5
+        kernelCube = getGaussCube(video, size(readimage(video, 1)));
+        temporalCube = getTemporalCube(video, k);
+        differentialImage = sum(double(temporalCube) .* kernelCube, 3);
+
+        
+
+        % Threshold values - above a THRESHOLD and the coresponding pixel should
+        % be white, otherwise, black.
+        mask = delta_threshold(differentialImage);
+
+        global WHITE
+        color = readimage(video, k);
+        movement = uint8(double(rgb2gray(color)) .* (mask/WHITE));
+
+        % Image
+        subplot(2,4,1);
+        imshow(color);  
+        title('Color')
+        
+        % Filter
+        subplot(2,4,2);
+        imshow(differentialImage); 
+        title('Delta')
+        
+        % Mask
+        subplot(2,4,3);
+        imshow(mask); % Multiply by WHITE value to get a binary image 
+        title('Gauss Mask')
+        
+        % Show gray image of mask 
+        subplot(2,4,4);
+        imshow(uint8(movement));  % Display mask.
+        title('Gauss Movement')
+
+        subplot(2, 4, [5, 6]);
+        display(std2(differentialImage));
+        scatter(k, std2(differentialImage), 'r');
+        title('Standard Deviation of Differntial')
+        xlabel('Frame number')
+        ylabel('STD')
+        hold on;
+        
+        subplot(2, 4, [7, 8]);
+        scatter(k, (mean2(differentialImage .* differentialImage)), 'r');
+        title('Mean Squared Value of Differential')
+        xlabel('Frame number')
+        ylabel('Mean Squared')
+        hold on;
+        drawnow;
+    end
+
+end
+
+function gaussCube = getGaussCube(video, imageSize)
+    gauss = [1 4 6 4 1] * (1/16);
+    differential = [-1 0 1];
+    kernel = conv(gauss, differential, 'same');
+    differentialImage = ones(imageSize(1), imageSize(2));
+    gaussCube = repmat(kernel, [imageSize(1) * imageSize(2), 1]);
+    gaussCube = reshape(gaussCube, [imageSize(1), imageSize(2), length(kernel)]);
+end
+
+function slice = getTemporalCube(video, k, m, n)
+    slice = [];
+    for i = 1 : 5
+        slice = cat(3, slice, rgb2gray(readimage(video, k - 3 + i)));
     end
 end
 
 function [filter, mask, movement] = ... 
     processing_pipeline_helper(color, previous, next, filter_selector)
-        [previous_frame, next_frame] ...
-        = apply_filter(previous, next, filter_selector);
+    % Apply spatial filter, threshold values, apply mask
+        
+    [previous_frame, next_frame] ...
+        = apply_spatial_filter(previous, next, filter_selector);
 
-        %imshowpair(previous_frame, next_frame, 'montage')
+    %imshowpair(previous_frame, next_frame, 'montage')
 
-        filter = rgb2gray(next_frame);
-        delta = rgb2gray(next_frame) - rgb2gray(previous_frame);
+    filter = rgb2gray(next_frame);
+    delta = rgb2gray(next_frame) - rgb2gray(previous_frame);
 
-        % Threshold values - above a THRESHOLD and the coresponding pixel should
-        % be white, otherwise, black.
-        mask = delta_threshold(delta);
+    % Threshold values - above a THRESHOLD and the coresponding pixel should
+    % be white, otherwise, black.
+    mask = delta_threshold(delta);
 
-        global WHITE
-        movement = rgb2gray(color) .* (mask/WHITE);
+    global WHITE
+    movement = rgb2gray(color) .* (mask/WHITE);
 end
 
-function [previous_frame, next_frame] = apply_filter(previous, next, selector)
+function [previous_frame, next_frame] = apply_spatial_filter(previous, next, selector)
 % Applies the requested filter to the given frames. returns the frames.
     global BOX3
     global BOX5
@@ -110,8 +184,8 @@ function mask = delta_threshold(frame)
     global BLACK
     global THRESHOLD
 
-    frame(frame >= THRESHOLD) = WHITE;
-    frame(frame < THRESHOLD) = 0;
+    frame(abs(frame) >= THRESHOLD) = WHITE;
+    frame(abs(frame) < THRESHOLD) = 0;
 
     mask = frame;
 end
