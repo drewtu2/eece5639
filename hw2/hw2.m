ab50b5d26159a6b251cc93261edebc7926a72605
Andrew Tu <drewtu2@yahoo.com>
2018-05-24 16:43:15 -0400

    updated hw2

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -2,17 +2,21 @@
 function hw2()
     global images;
     images = genImages(10);
+    showImage = false;
     
-    for ii = 1:size(images, 3)
-        %display(strcat('Image ', num2str(ii)));
-        imshow(images(:, :, ii));
-        drawnow;
+    if showImage
+        for ii = 1:size(images, 3)
+            %display(strcat('Image ', num2str(ii)));
+            imshow(images(:, :, ii));
+            drawnow;
+        end
     end
+    
     global noise
     noise = EST_NOISE(images);
     
     display('**************************************');
-    display('Filtering Images')
+    display('Problem 1-3: Filtering Images')
     display('**************************************');
     
     displayNoise('Noisy Images:', mean2(noise));
@@ -29,7 +33,10 @@ function hw2()
     gauss2.noise = EST_NOISE(gauss2.filtered);
     displayNoise('Gauss Filtered Separable:', mean2(gauss2.noise));
 
-    averagingFilters(false);
+    averagingFilters(showImage);
+    saltPepperImage();
+    medianFilter();
+    stepProfile();
 end
 
 function displayNoise(type, noise)
@@ -38,6 +45,7 @@ function displayNoise(type, noise)
 end
 
 function images = genImages(numImages)
+% Problem 1
 % Generates numImages images of color gray level 128 with zero mean
 % gaussian noise with standard deviation 2. 
 % Returns cube with the image on XY plane and the stack of images going
@@ -52,6 +60,7 @@ function images = genImages(numImages)
 end
 
 function filtered = boxCube(cube, n)
+% Problem 2
 % Applies an n by n box filter to the given cube. Cube consists of multiple
 % frames. Returns the filtered result. 
     filtered = [];
@@ -63,13 +72,17 @@ function filtered = boxCube(cube, n)
 end
 
 function filtered = box(frame, n)
+% Problem 2
 % Applies an n by n box filter to the given frame.
 % Returns the filtered result. 
     filtered = imfilter(frame, (1/(n * n)) * ones(n));
 end
 
 function filtered = gaussCube(cube, n, sigma)
-% Applies an n by n box filter to the given cube.
+% Problem 3
+%
+% Applies an n by n gauss filter to the given cube using the 2D gaussian 
+% filter.
     filtered = [];
     for frameNum = 1:size(cube, 3)
         frame = cube(:, :, frameNum);
@@ -97,7 +110,9 @@ function filtered = gaussian(frame, n, sigma)
 end
 
 function filtered = gaussCube2(cube, n, sigma)
-% Applies an n by n box filter to the given cube.
+% Problem 3
+% Applies an n by n gauss filter to the given cube using the two 1D 
+% gaussian filters.
     filtered = [];
     for frameNum = 1:size(cube, 3)
         frame = cube(:, :, frameNum);
@@ -107,6 +122,10 @@ function filtered = gaussCube2(cube, n, sigma)
 end
 
 function filtered = gaussianSeparable(frame, k, sigma)
+    % Problem 3
+    % Creates two 1D Masks that can be convolved to produce a 2D gaussian
+    % filter
+    
     % Generate horizontal and vertical co-ordinates, where
     % the origin is in the middle
     ind = -floor(k/2) : floor(k/2);
@@ -135,6 +154,8 @@ function filtered = gaussianSeparable(frame, k, sigma)
 end
 
 function noise = EST_NOISE(images)
+% General funciton for estimating the noise as per Chapter 2 of Trucco and
+% Verri
 % Images is a data cube with XY being the representation of a single image
 % and Z being imagne number n. 
     E_Images = double(sum(images, 3) / size(images, 3));
@@ -149,6 +170,9 @@ function noise = EST_NOISE(images)
 end
 
 function averagingFilters(showImage)
+% Problem 4
+% Applies two different averaging filters to the image given in problem 4. 
+% Prints the filtered results. 
 
     filterA = 1/5 * [1 1 1 1 1];
     filterB = 1/10 * [1 2 3 2 1];
@@ -168,10 +192,106 @@ function averagingFilters(showImage)
     end
     
     display('**************************************');
-    display('Averaging Filters')
+    display('Problem 4: Averaging Filters')
     display('**************************************');
     display(strcat('Original image: ', mat2str(image)));
     display(strcat('Filtered Uniform : ', mat2str(filteredUniform)));
     display(strcat('Filtered Gaussian: ', mat2str(filteredGaussian)));
 
+end
+
+function saltPepperImage()
+% Problem 5
+% Finds the PDF for the pixels surrounding a gray line on a salt and pepper
+% background. 
+    PEPPER = 100;
+    SALT = 0;
+    GRAY = 50;
+    
+    % Create the salt and pepper background
+    image = rand(500, 5);
+    image(image >= .7) = PEPPER;
+    image(image < .7) = SALT;
+    
+    % Draw the vertical line 
+    for row = 1:size(image, 1)
+       image(row, ceil(size(image,2)/2)) = GRAY;
+    end
+    
+    figure;
+    subplot(1, 2, 1);
+    imshow(image);
+    filter = [-1, 2, -1];
+    filterResults = imfilter(image, filter, 'same');
+    resultsOfInterest = cat(1, filterResults(:, 2), filterResults(:, 4));
+    
+    subplot(1, 2, 2);
+    pie(categorical(resultsOfInterest));
+    title('Problem 5: Distribution of values surrounding vertical line');
+
+end
+
+function medianFilter()
+% Problem 6
+% Filters the image described in problem 6 using a 3x3 median filter. 
+    % Create image
+    image = zeros(8, 8);
+    
+    for i = 1:8
+        for j = 1:8
+            image(i, j) = abs((i - 1) - (j -1));
+        end
+    end
+    
+    % Filter
+    filtered = zeros(8, 8);
+    filtered(1, :) = image(1, :);
+    filtered(8, :) = image(8, :);
+    filtered(:, 1) = image(:, 1);
+    filtered(:, 8) = image(:, 8);
+    
+    for i = 2:7
+        for j = 2:7
+            filtered(i, j) = median9(image, i, j);
+        end 
+    end
+    
+    function med = median9(image, i, j)
+        values = zeros(3, 3);
+        
+        for i2 = -1:1
+            for j2 = -1:1
+                values(i2 + 2, j2 + 2) = image(i + i2, j + j2);
+            end
+        end
+        
+        med = median(values(:));
+    end
+    display('**************************************');
+    display('Problem 6: Median Filters')
+    display('**************************************');
+    image
+    filtered
+end
+
+function stepProfile() 
+% Problem 7
+% Filters a step image of with both an averaging filter and a median
+% filter. Median filter doesn't change the step, averaging filter does. 
+    image = [4 4 4 4 8 8 8 8];
+    
+    filter = 1/4 * [1 2 1];
+    average = conv(image, filter, 'same');
+    
+    medianFiltered = image;
+    for i = 2:length(image) - 1
+        medianFiltered(i) = median([image(i - 1), image(i), image(i + 1)]);
+    end
+    
+    display('**************************************');
+    display('Problem 7: 1D Step Profile')
+    display('**************************************');
+    average         % Blends the step. 
+    medianFiltered  % Remains unchanged
+    
 end
